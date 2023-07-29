38359bf3ec4ed2d788bfa6b0d5323542b57a496d
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-07-29 20:57:55 -0300

    remove comment

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -5,7 +5,6 @@ parse.add_argument('file', help='the file name')
 args = parse.parse_args()
 fn = args.file
 
-"""
 try:
     f = open(fn, "r")
     new_f = open(fn.replace('.p', '.cs'), "w")
@@ -15,10 +14,3 @@ try:
     new_f.write(text)
 except:
     print("the file doens't exist in local")
-"""
-f = open(fn, "r")
-new_f = open(fn.replace('.p', '.cs'), "w")
-text: str = ''
-if f.readlines().__contains__('console.write'):
-    text = f.readlines().replace('console.write','Console.Write')
-new_f.write(text)

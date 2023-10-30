e86d5adbeb4e3e770c3a5d749f07241c8c8ca8d7
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-10-30 23:41:56 +0000

    try except structure

--- runner/runner.py
+++ runner/runner.py
@@ -2,3 +2,9 @@ import argparse
 
 parse = argparse.ArgumentParser()
 parse.add_argument("file", help="the compiled file, must be encoded")
+args = parse.parse_args()
+
+try:
+    compiled = open(args.file, "w")
+except:
+    print("a error happens while uncoding the file")

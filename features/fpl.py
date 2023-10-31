69583e624ac6ce3056bf634afbb7d4f4fffcab2e
esdraslov <edvan.marcelino56@gmail.com>
2023-10-31 14:46:05 -0300

    fixed all incorrect spelling words

--- features/fpl.py
+++ features/fpl.py
@@ -3,8 +3,8 @@ import argparse
 parse = argparse.ArgumentParser()
 action_help = """
 commands:\n
- install - install a feture\n
- upgrade - install a upgrade for a feture\n
+ install - install a feature\n
+ upgrade - install a upgrade for a feature\n
 """
 action = parse.add_argument('action', help=action_help)
 package = parse.add_argument('package', help="the package to be installed")

e60df89cdc3fdd04cb95e9f4d3165465ceef08c9
esdraslov <edvan.marcelino56@gmail.com>
2023-10-31 15:06:30 -0300

    added lines and a for in

--- runner/runner.py
+++ runner/runner.py
@@ -5,7 +5,11 @@ parse.add_argument("file", help="the compiled file, must be encoded")
 args = parse.parse_args()
 
 try:
-    compiled = open(args.file, "w")
+    compiled = open(args.file, "r")
+    lines: list = compiled.readlines()
+
+    for line in lines:
+        pass
 except:
     print("a error happens while uncoding the file")
 finally:

61f2a7513e6c481ace46044cf7c142a53cca21f3
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-10-31 00:08:48 +0000

    json-server installed (1K+) and feature...

--- fetures/fpl.py
+++ fetures/fpl.py
@@ -3,8 +3,9 @@ import argparse
 parse = argparse.ArgumentParser()
 action_help = """
 commands:\n
--i install - install a feture\n
--u upgrade - install a upgrade for a feture\n
+ install - install a feture\n
+ upgrade - install a upgrade for a feture\n
 """
 action = parse.add_argument('action', help=action_help)
+package = parse.add_argument('package', help="the package to be installed")
 args = parse.parse_args()

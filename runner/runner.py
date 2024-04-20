dc09bd9be394a06f07e795c1a58910c1b241d14e
esdraslov <edvan.marcelino56@gmail.com>
2024-04-20 12:48:24 -0300

    Pointer + GET fix

--- runner/runner.py
+++ runner/runner.py
@@ -14,6 +14,7 @@ try:
         number = 0b00000000 # P works in 1 byte every number of bits
         RAMA = [] # RAM (arrays is the RAM, because array is directed separeted in RAM)
         SETV = number # Number for some actions (multi-parameters)
+        PNTV = 0b00000000 # The pointer of SETV, RAMA or themself
         act = "" # action
         for i, k in enumerate(kw):
             if i % 2 == 1: # 2 % 2 != 0 so then ins't a keyword; 4 % 2 == 0 so then it's a keyword
@@ -27,9 +28,20 @@ try:
                 elif act == "EXIT":
                     sys.exit(0) # Exit successfully
                 elif act == "GET":
-                    print(RAMA[number]) # print an integer from RAM
+                    if number >= len(RAMA): # Check if is out of index
+                        print("Error [P:RAMIndex]: out of index")
+                        sys.exit(1)
+                    else:
+                        print(RAMA[number]) # print an integer from RAM
                 elif act == "oRAM":
                     RAMA.pop(int(number)) # remove an integer from RAM
+                elif act == "PNT":
+                    if SETV == 0b00000000:
+                        PNTV = number # Point to themself
+                    elif SETV == 0b00000001:
+                        PNTV = RAMA[number] # Point to RAMA
+                    else:
+                        PNTV = SETV # Point to SETV
                 else:
                     raise Exception(f"Unknown action [{act}], please check the code and try again, remember, all actions is uppercase written")
             else:
@@ -38,5 +50,5 @@ except Exception as error:
     print("Error [P::Exeception]: %s" % error)
     sys.exit(1)
 except:
-    print("Error [Unknown]")
+    print("Error [P::UnknownOrKnown]")
     sys.exit(1)

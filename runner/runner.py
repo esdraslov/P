bb601ebca5cabf423e0d2c6db53cf5c39aa47c7f
esdraslov <edvan.marcelino56@gmail.com>
2024-04-20 12:39:59 -0300

    comments

--- runner/runner.py
+++ runner/runner.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 
 parse = argparse.ArgumentParser()
-parse.add_argument("file", help="the compiled file, must be encoded")
+parse.add_argument("file", help="the compiled file, must be encoded") # must NOT be encoded
 args = parse.parse_args()
 
 try:
@@ -10,28 +10,33 @@ try:
     lines: list = compiled.readlines()
 
     for line in lines:
-        kw = line.split(" ")
+        kw = line.split(" ") # Every single keyword
         number = 0b00000000 # P works in 1 byte every number of bits
-        RAMA = []
-        SETV = number
-        act = ""
+        RAMA = [] # RAM (arrays is the RAM, because array is directed separeted in RAM)
+        SETV = number # Number for some actions (multi-parameters)
+        act = "" # action
         for i, k in enumerate(kw):
             if i % 2 == 1: # 2 % 2 != 0 so then ins't a keyword; 4 % 2 == 0 so then it's a keyword
-                number = int(k, base=2)
+                number = int(k, base=2) # the number passed as parameter
                 if act == "SET":
-                    SETV = number
-                if act == "RAM":
-                    RAMA.append(number)
-                if act == "PUT":
+                    SETV = number # set a number for some actions
+                elif act == "RAM":
+                    RAMA.append(number) # add number to RAM
+                elif act == "PUT":
                     print(SETV) # Aways need to be 00000000 (0) bits as parameter
-                if act == "EXIT":
-                    sys.exit(0)
-                if act == "GET":
-                    print(RAMA[number])
-                if act == "oRAM":
-                    RAMA.pop(int(number))
+                elif act == "EXIT":
+                    sys.exit(0) # Exit successfully
+                elif act == "GET":
+                    print(RAMA[number]) # print an integer from RAM
+                elif act == "oRAM":
+                    RAMA.pop(int(number)) # remove an integer from RAM
+                else:
+                    raise Exception(f"Unknown action [{act}], please check the code and try again, remember, all actions is uppercase written")
             else:
                 act = k
+except Exception as error:
+    print("Error [P::Exeception]: %s" % error)
+    sys.exit(1)
 except:
-    print("Error")
+    print("Error [Unknown]")
     sys.exit(1)
